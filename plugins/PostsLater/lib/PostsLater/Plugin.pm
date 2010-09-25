bb4b3e49df93c418296a0aed5965aa5d7605e28d
David Raynes <rayners@gmail.com>
2010-09-25 00:42:08 -0400

    First pass at load/save of the post_day values

--- plugins/PostsLater/lib/PostsLater/Plugin.pm
+++ plugins/PostsLater/lib/PostsLater/Plugin.pm
@@ -4,16 +4,26 @@ package PostsLater::Plugin;
 use strict;
 use warnings;
 
+my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
 sub load_config {
     my $plugin = shift;
     my ( $args, $scope ) = @_;
 
     $plugin->SUPER::load_config(@_);
+
+    for my $d (@days) {
+        $args->{ 'post_day_' . lc($d) } = $args->{post_day}->{ lc($d) };
+    }
 }
 
 sub save_config {
     my $plugin = shift;
     my ( $args, $scope ) = @_;
+
+    for my $d (@days) {
+        $args->{post_day}->{ lc($d) } = $args->{ 'post_day_' . lc($d) };
+    }
 
     $plugin->SUPER::save_config(@_);
 }
